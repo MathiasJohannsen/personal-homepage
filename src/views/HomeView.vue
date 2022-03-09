157ae42438b4c3dc03ff079d3690e1b614ae3a1f
Mathias Johannsen <m.johannsen.privat@gmail.com>
2022-03-10 00:06:48 +0100

    Navbar with animation

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,6 +13,79 @@
       </div>
     </div>
   </div>
+  <div class="fixed top-0 left-0 right-0">
+    <div class="flex justify-center">
+      <router-link
+        to="/about"
+        class="text-gray-900 pt-10 pb-3 w-0 tracking-full-width font-bold"
+      >
+        <span>H</span>
+        <span>O</span>
+        <span>M</span>
+        <span>E</span>
+      </router-link>
+    </div>
+  </div>
+  <div class="fixed bottom-0 left-0 right-0">
+    <div class="flex justify-center">
+      <p class="text-gray-900 pt-3 pb-10 w-0 tracking-full-width font-bold">
+        <span>C</span>
+        <span>O</span>
+        <span>N</span>
+        <span>T</span>
+        <span>A</span>
+        <span>C</span>
+        <span>T</span>
+      </p>
+    </div>
+  </div>
+  <div class="fixed right-0 top-0 bottom-0">
+    <div class="flex items-center h-screen">
+      <router-link
+        to="/about"
+        class="
+          text-gray-900
+          pr-10
+          pl-3
+          tracking-full-height
+          turn-left
+          font-bold
+          flex-col-reverse
+        "
+      >
+        <span>P</span>
+        <span>R</span>
+        <span>O</span>
+        <span>J</span>
+        <span>E</span>
+        <span>C</span>
+        <span>T</span>
+        <span>S</span>
+      </router-link>
+    </div>
+  </div>
+  <div class="fixed left-0 top-0 bottom-0">
+    <div class="flex items-center h-screen">
+      <router-link
+        to="/about"
+        class="
+          text-gray-900
+          pr-3
+          pl-10
+          tracking-full-height
+          turn-right
+          font-bold
+          flex-col
+        "
+      >
+        <span>A</span>
+        <span>B</span>
+        <span>O</span>
+        <span>U</span>
+        <span>T</span>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -22,8 +95,49 @@ import HelloWorld from "@/components/HelloWorldComponent.vue"; // @ is an alias 
 export default defineComponent({
   name: "HomeView",
   components: {
-    // eslint-disable-next-line vue/no-unused-components
     HelloWorld,
   },
 });
 </script>
+
+
+<style scoped>
+.tracking-full-width {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  transition-duration: 0.7s;
+}
+
+.tracking-full-width span {
+  padding-inline: 5px;
+}
+
+:hover.tracking-full-width {
+  width: 100vw;
+  color: red;
+}
+
+.tracking-full-height {
+  display: flex;
+  justify-content: space-around;
+  transition-duration: 0.7s;
+  height: 0px;
+}
+
+.tracking-full-height span {
+  padding-inline: 5px;
+}
+
+:hover.tracking-full-height {
+  height: 100vh;
+  color: red;
+}
+
+.turn-right span {
+  transform: rotate(90deg);
+}
+.turn-left span {
+  transform: rotate(-90deg);
+}
+</style>
